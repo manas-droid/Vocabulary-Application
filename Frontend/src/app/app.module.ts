253a705db7068ea68b12247e9b05428e2402d0f2
Manas Kalangan <manaska@amdocs.com>
2022-11-19 01:04:02 +0530

    Designed Login Component UI

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'vocabulary-community',
+    appId: '1:690182914216:web:880e7b411523976ed7829d',
+    storageBucket: 'vocabulary-community.appspot.com',
+    apiKey: 'AIzaSyBvuRI-M33v8JGhdojRTE-uaI0LBVQPVfM',
+    authDomain: 'vocabulary-community.firebaseapp.com',
+    messagingSenderId: '690182914216',
+    measurementId: 'G-M9XPXF9H0Q',
+  },
   production: true
 };
